85fe7c86d8cdef48b9146470df82d60746faeab0
Brandon McGinty-Carroll <bmmcginty@bmcginty.us>
2019-09-29 17:12:37 -0400

    commit changes from f123 project for barnard-sound and barnard-ui

--- extras/barnard-sound.sh
+++ extras/barnard-sound.sh
@@ -24,12 +24,19 @@
                                                                                                                                                                 
 # 1 is off, 0 is on
 notify=0
+sound=0
+
+# Select notify type, notify-send or fenrir
+notifyType="fenrir"
+
 connect() {
-    play -n synth .05 pl 1050 pl 1100 remix - pad 0 .05 repeat
+    [[ $sound ]] && play -n synth .05 pl 1050 pl 1100 remix - pad 0 .05 repeat
+    [[ $notify ]] && notify "${1}ed."
 }
 
 disconnect() {
-    play "|sox -np synth .1 sin 350 sin 440 norm -4 pad 0 .1 repeat 10 remix -" fade p 0 1 .5
+    [[ $sound ]] && play "|sox -np synth .1 sin 350 sin 440 norm -4 pad 0 .1 repeat 10 remix -" fade p 0 1 .5
+    [[ $notify ]] && notify "${1}ed."
 }
 
 is_function() {
@@ -37,32 +44,50 @@ is_function() {
 }
 
 join() {
-    play "|sox -np synth .04 sin 1400 sin 2060 sin 2450 sin 2600 norm -8 remix - pad 0 .02 repeat 25" fade p 0 .75 .5
+    [[ $sound ]] && play "|sox -np synth .04 sin 1400 sin 2060 sin 2450 sin 2600 norm -8 remix - pad 0 .02 repeat 25" fade p 0 .75 .5
+    [[ $notify ]] && notify "$2 ${1}ed the channel."
 }
 
 leave() {
-    play -n synth .5 sin 480 sin 620 remix - norm -8 pad 0 0.5 repeat
+    [[ $sound ]] && play -n synth .5 sin 480 sin 620 remix - norm -8 pad 0 0.5 repeat
+    [[ $notify ]] && notify "$2 left the channel."
 }
 
 micdown() {
-    play -qnV0 synth .25 sin G6:E5  norm -8
+    [[ $sound ]] && play -qnV0 synth .25 sin G6:E5  norm -8
+    [[ $notify ]] && notify "You have stopped transmitting."
 }
 
 micup() {
-    play -qnV0 synth .25 sin E5:G6  norm -8
+    [[ $sound ]] && play -qnV0 synth .25 sin E5:G6  norm -8
+    [[ $notify ]] && notify "You are now transmitting."
 }
 
 msg() {
-    play -n synth .3 sin 1290:1490 sin 1494:1294 remix - norm -8
+    [[ $sound ]] && play -n synth .3 sin 1290:1490 sin 1494:1294 remix - norm -8
+    [[ $notify ]] && notify "$1 from $2: $3"
+}
+
+notify() {
+if [[ "$notifyType" == "notify-send" ]]; then
+    notify-send "$@"
+else
+    if [[ -e "/tmp/fenrirscreenreader-deamon.sock" ]]; then
+        socatFile="/tmp/fenrirscreenreader-deamon.sock"
+    else
+        socatFile="$(find /tmp/ -maxdepth 1 -type s -name 'fenrirscreenreader-*.sock' | head -1)"
+    fi
+    echo "command say $@" | socat - UNIX-CLIENT:$socatFile
+fi
 }
 
 pm() {
-    play -n synth .5 sin 440 sin 480 remix - norm -8
+    [[ $sound ]] && play -n synth .5 sin 440 sin 480 remix - norm -8
+    [[ $notify ]] && notify "$1 from $2: $3"
 }
 
 if is_function "$1" ; then
-    eval "$1" &> /dev/null
-    [[ $notify ]] && notify-send "$2: $1"
+    eval "$1" "$1" "$2" "$3" &> /dev/null
 else
     echo "The given barnard event has not yet been added."
 fi
